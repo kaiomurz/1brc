6e06531196f0c262213b9e2a914d1496f07b13f8
kaio.motawara1 <kaio.motawara1@nhs.net>
2024-07-02 15:32:44 +0000

    complete basic algorithm

--- onebrc.py
+++ onebrc.py
@@ -1,5 +1,37 @@
-
-def get_city_results(city:str, results:list[int])->str:
-    city_min, city_mean, city_max = min(results), sum(results)/len(results), max(results)
+def get_city_results(city: str, city_list: list[float]) -> str:
+    """takes a tuple like ('Mumbai',[10.0,20.0,30.0,40.0]) and returns a string 
+    like 'Mumbai;10.0;25.0;40.0' where the three numbers are the min, mean, and 
+    max of the list."""
+    city_min, city_mean, city_max = min(city_list), sum(city_list)/len(city_list), max(city_list)
     return f"{city};{city_min};{city_mean};{city_max}"
 
+
+def create_cities_dict(cities:list[str])->dict[str,list[float]]:
+    """ takes a list of form ['Mumbai;20','Mumbai;30','Mumbai;40','Mumbai;50']
+    returns dict of form {'Mumbai':[10,20,30,40,50]}
+    """
+    cities_dict:dict[str,list[int]] = {}
+    for row in cities:
+        city, str_temperature = row.split(";")
+        temperature = float(str_temperature)
+        if city in cities_dict:
+            cities_dict[city].append(temperature)
+        else:
+            cities_dict[city] = [temperature]
+    return cities_dict
+
+def get_results_list(cities_dict:dict[str,list[float]])->list[str]:
+    return [get_city_results(city, cities_dict[city]) for city in cities_dict]
+
+
+if __name__=="__main__":
+
+    with open("test-measurements.txt", "r") as f:
+        cities = f.readlines()
+
+    print(cities)
+
+    cities_dict = create_cities_dict(cities)
+    print(cities_dict)
+    results_list = get_results_list(cities_dict=cities_dict)
+    print(results_list)
--- test_onebrc.py
+++ test_onebrc.py
@@ -1,9 +1,30 @@
 from hamcrest import assert_that, equal_to
-from onebrc import get_city_results
+from onebrc import get_city_results, create_cities_dict
+
 
 def test_get_city_results():
-    city = 'Mumbai'
-    results = [10.0,20.0,30.0,40.0]
-    city_result = "Mumbai;10.0;25.0;40.0"
-    returned_city_result = get_city_results(city=city, results=results) 
-    assert_that(returned_city_result, equal_to(city_result))
+    city = "Mumbai"
+    city_list = [10.0, 20.0, 30.0, 40.0]
+    expected_city_result = "Mumbai;10.0;25.0;40.0"
+    returned_city_result = get_city_results(city=city, city_list=city_list)
+    assert_that(returned_city_result, equal_to(expected_city_result))
+
+
+def test_create_cities_dict():
+    cities = [
+        "Mumbai;20.0",
+        "Mumbai;30.0",
+        "Mumbai;40.0",
+        "Mumbai;50.0",
+        "Oman;30.5",
+        "Oman;40.5",
+        "London;20.5",
+    ]
+    expected_cities_dict = {
+        "Mumbai": [20.0, 30.0, 40.0, 50.0],
+        "Oman": [30.5, 40.5],
+        "London": [20.5],
+    }
+
+    returned_cities_dict = create_cities_dict(cities=cities) 
+    assert_that(returned_cities_dict, equal_to(expected_cities_dict))
